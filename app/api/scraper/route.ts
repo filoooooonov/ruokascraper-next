1aee802605f9caa4e59712233c3f353b95c06960
Aleksei Filonov <alexfiloonov@gmail.com>
2024-07-10 08:12:13 +0300

    Try all puppeteer stealth evasions with API route

--- app/api/scraper/route.ts
+++ app/api/scraper/route.ts
@@ -2,6 +2,22 @@ import { Item, ProductData } from "@/app/page";
 import { NextResponse } from "next/server";
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+require("puppeteer-extra-plugin-stealth/evasions/chrome.app");
+require("puppeteer-extra-plugin-stealth/evasions/chrome.csi");
+require("puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes");
+require("puppeteer-extra-plugin-stealth/evasions/chrome.runtime");
+require("puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow");
+require("puppeteer-extra-plugin-stealth/evasions/media.codecs");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.languages");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.permissions");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.plugins");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.vendor");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.webdriver");
+require("puppeteer-extra-plugin-stealth/evasions/sourceurl");
+require("puppeteer-extra-plugin-stealth/evasions/user-agent-override");
+require("puppeteer-extra-plugin-stealth/evasions/webgl.vendor");
+require("puppeteer-extra-plugin-stealth/evasions/window.outerdimensions");
 puppeteer.use(StealthPlugin());
 
 export async function POST(req: Request) {
